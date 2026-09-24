090518f0df774fc8e05a5cd583f05e61f65ffbd0
AlexZzk <41469204+AlexZzk@users.noreply.github.com>
2026-09-24 11:33:17 +0800

    Create index.ts

---
+++ src/layouts/index.ts
@@ -0,0 +1,3 @@
+export { default as SidebarLayout } from './sidebar/SidebarLayout.vue'
+export { default as TopLayout } from './top/TopLayout.vue'
+export { default as BlankLayout } from './blank/BlankLayout.vue'
